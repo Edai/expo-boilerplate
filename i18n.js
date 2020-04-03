061ec7cca1f3d10aa9c533b31f1a41056c2665bf
Valentin KAO <valentin.kao@unabiz.com>
2020-04-03 12:02:30 +0800

    Init: Support Internationalization

---
+++ i18n.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import * as Localization from "expo-localization";
+import i18n from "i18n-js";
+
+const enTranslationMessages = require("./translations/en.json");
+const translationMessages = {
+  en: enTranslationMessages
+};
+
+i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+i18n.translations = translationMessages;
+i18n.getText = str => {
+  return i18n.t(str);
+};
+export default IntlContext = createContext(i18n);
